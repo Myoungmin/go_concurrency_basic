4350f632afce0fc4cb582969b1681dc8dc3999b1
AzureAD\고명민 <miung222@gmail.com>
2023-11-17 22:12:40 +0900

    WaitGroup 프로젝트 시작
    Attacked evil ninja가 출력되기도 하고 안되기도한다.

--- main.go
+++ main.go
@@ -2,45 +2,13 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	ninja1, ninja2 := make(chan string), make(chan string)
-
-	go captainElect(ninja1, "Ninja 1")
-	go captainElect(ninja2, "Ninja 2")
-
-	select {
-	case message := <-ninja1:
-		fmt.Println(message)
-	case message := <-ninja2:
-		fmt.Println(message)
-	default:
-		fmt.Println("Neither")
-	}
-	roughlyFair()
+	go attack("Tommy")
+	fmt.Println("Mission completed")
 }
 
-func captainElect(ninja chan string, message string) {
-	time.Sleep(3 * time.Second)
-	ninja <- message
-}
-
-func roughlyFair() {
-	ninja1 := make(chan interface{})
-	close(ninja1)
-	ninja2 := make(chan interface{})
-	close(ninja2)
-
-	var ninja1Count, ninja2Count int
-	for i := 0; i < 1000; i++ {
-		select {
-		case <-ninja1:
-			ninja1Count++
-		case <-ninja2:
-			ninja2Count++
-		}
-	}
-	fmt.Printf("ninja1Count: %d, ninja2Count: %d\n", ninja1Count, ninja2Count)
+func attack(evilNinja string) {
+	fmt.Println("Attacked evil ninja:", evilNinja)
 }
