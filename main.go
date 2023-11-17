c0df585fe353e485dd994fd46f1a8cb607b343f4
AzureAD\고명민 <miung222@gmail.com>
2023-11-18 08:59:55 +0900

    sync.WaitGroup으로 goroutine 실행 후 종료되도록 동기화 맞추기

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
 func main() {
-	go attack("Tommy")
+	var beeper sync.WaitGroup
+	beeper.Add(1)
+	go attack("Tommy", &beeper)
+	beeper.Wait()
 	fmt.Println("Mission completed")
 }
 
-func attack(evilNinja string) {
+func attack(evilNinja string, beeper *sync.WaitGroup) {
 	fmt.Println("Attacked evil ninja:", evilNinja)
+	beeper.Done()
 }
