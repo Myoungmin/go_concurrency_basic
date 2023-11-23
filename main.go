63e4051682910fa69ddf65d99253d42166f05646
AzureAD\고명민 <miung222@gmail.com>
2023-11-23 21:49:14 +0900

    WaitGroup의 deadLock과 negative WaitGroup couter

--- main.go
+++ main.go
@@ -1,24 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"sync"
 )
 
 func main() {
+	// var beeper sync.WaitGroup
+	// evilNinjas := []string{"Tommy", "Johnny", "Bobby"}
+	// beeper.Add(len(evilNinjas))
+
+	// for _, evilNinja := range evilNinjas {
+	// 	go attack(evilNinja, &beeper)
+	// }
+
+	// beeper.Wait()
+	// fmt.Println("Mission completed")
+
 	var beeper sync.WaitGroup
-	evilNinjas := []string{"Tommy", "Johnny", "Bobby"}
-	beeper.Add(len(evilNinjas))
-
-	for _, evilNinja := range evilNinjas {
-		go attack(evilNinja, &beeper)
-	}
-
-	beeper.Wait()
-	fmt.Println("Mission completed")
-}
-
-func attack(evilNinja string, beeper *sync.WaitGroup) {
-	fmt.Println("Attacked evil ninja:", evilNinja)
+	beeper.Add(1)
+	// 1개만 Done 되어야한다.
+	// 없으면 deadLock, 2개 이상되면 negative WaitGroup couter
 	beeper.Done()
+	//beeper.Done()
+	beeper.Wait()
 }
+
+// func attack(evilNinja string, beeper *sync.WaitGroup) {
+// 	fmt.Println("Attacked evil ninja:", evilNinja)
+// 	beeper.Done()
+// }
