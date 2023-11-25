ee607256516b015f326f060c2cbeaad85b8c5c35
AzureAD\고명민 <miung222@gmail.com>
2023-11-26 08:52:12 +0900

    sync.RWMutex로 읽기 잠금과 쓰기 잠금 예시

--- main.go
+++ main.go
@@ -7,11 +7,17 @@ import (
 )
 
 var (
-	lock  sync.Mutex
-	count int
+	lock   sync.Mutex
+	reLock sync.RWMutex
+	count  int
 )
 
 func main() {
+	//basics()
+	readAndWrite()
+}
+
+func basics() {
 	iterations := 1000
 	for i := 0; i < iterations; i++ {
 		go increment()
@@ -24,4 +30,30 @@ func increment() {
 	lock.Lock()
 	count++
 	lock.Unlock()
+}
+
+func readAndWrite() {
+	go read()
+	go write()
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Done")
+}
+
+func read() {
+	reLock.RLock()
+	defer reLock.RUnlock()
+
+	fmt.Println("Read locking")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Read unlocking")
+}
+
+func write() {
+	reLock.Lock()
+	defer reLock.Unlock()
+
+	fmt.Println("Write locking")
+	time.Sleep(1 * time.Second)
+	fmt.Println("Writing unlocking")
 }
